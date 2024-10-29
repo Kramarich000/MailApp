b50f7f963d5de8a5cd44475d2a06f0814b2dbe76
Karen <ipepela1@mail.ru~>
2024-10-29 23:11:04 +0300

    отправка сообщений

---
+++ MailService.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public void SendMessage()
+{
+    if (currentUser == null)
+    {
+        Console.WriteLine("Сначала зарегистрируйтесь.");
+        return;
+    }
+
+    Console.Write("Введите сообщение: ");
+    string message = Console.ReadLine();
+    currentUser.SaveMessage(message);
+    Console.WriteLine("Сообщение отправлено.");
+}
+
+public void ViewMessages()
+{
+    if (currentUser == null)
+    {
+        Console.WriteLine("Сначала зарегистрируйтесь.");
+        return;
+    }
+
+    Console.WriteLine("Ваши сообщения:");
+    var messages = currentUser.LoadMessages();
+
+    foreach (var msg in messages)
+    {
+        Console.WriteLine(msg);
+    }
+}
