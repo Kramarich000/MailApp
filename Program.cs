f6dd9cea2e1dee8eb48007db20354f547a2eacb4
Karen <ipepela1@mail.ru~>
2024-10-29 23:09:26 +0300

    основная функция

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MailApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mailService = new MailService();
+            string input;
+
+            Console.WriteLine("Добро пожаловать в почтовое приложение!");
+
+            while (true)
+            {
+                Console.WriteLine("Введите 'register' для регистрации или 'exit' для выхода:");
+                input = Console.ReadLine().ToLower();
+
+                if (input == "exit") break;
+
+                if (input == "register")
+                {
+                    mailService.RegisterUser();
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда.");
+                }
+            }
+        }
+    }
+}
