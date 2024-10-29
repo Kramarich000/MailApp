23085545fc0740bb7dec174a920d00bd6b4c62d9
Karen <ipepela1@mail.ru~>
2024-10-29 23:09:40 +0300

    польватель и мэйл

---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MailApp
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<string> Messages { get; set; } = new List<string>();
+
+        public User(string username)
+        {
+            Username = username;
+        }
+
+        public void SaveMessage(string message)
+        {
+            Messages.Add(message);
+            File.AppendAllText("messages.txt", $"{Username}: {message}\n");
+        }
+
+        public List<string> LoadMessages()
+        {
+            if (File.Exists("messages.txt"))
+            {
+                return new List<string>(File.ReadAllLines("messages.txt"));
+            }
+            return new List<string>();
+        }
+    }
+}
